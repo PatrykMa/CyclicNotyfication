e371fe62add4d3345dc58b9d5ec22c3fd6d3267d
PatrykMa <wiatraczekk@gmail.com>
2019-09-25 19:38:03 +0200

    migrate to binding

--- app/src/main/java/com/example/cyclicnotyfication/MainActivity.kt
+++ app/src/main/java/com/example/cyclicnotyfication/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.cyclicnotyfication
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cyclicnotyfication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.buttonStart.setOnClickListener {
+
+        }
     }
 
 
