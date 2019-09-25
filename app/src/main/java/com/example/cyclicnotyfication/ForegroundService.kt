248a9f0508d2a3220907dff1774cc24577587515
PatrykMa <wiatraczekk@gmail.com>
2019-09-25 20:22:29 +0200

    Add creating new notification

--- app/src/main/java/com/example/cyclicnotyfication/ForegroundService.kt
+++ app/src/main/java/com/example/cyclicnotyfication/ForegroundService.kt
@@ -10,18 +10,24 @@ import android.app.Service
 import android.app.NotificationManager
 import android.app.NotificationChannel
 import android.os.Build
-
-
+import android.os.SystemClock
+import android.support.annotation.UiThread
+import android.support.v4.app.NotificationManagerCompat
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.notificationManager
+import org.jetbrains.anko.uiThread
 
 
 class ForegroundService : Service() {
 
     companion object {
         val CHANNEL_ID = "ForegroundServiceChannel"
+        val CHANNEL_ID_CHILD = "ForegroundServiceChannelCHILD"
+        private var  isRunning = false;
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
-        val input = intent.getStringExtra("inputExtra")
+        val input = intent.getIntExtra("time",15)
         createNotificationChannel()
         val notificationIntent = Intent(this, MainActivity::class.java)
         val pendingIntent = PendingIntent.getActivity(
@@ -31,12 +37,34 @@ class ForegroundService : Service() {
 
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("Foreground Service")
-            .setContentText(input)
+            .setContentText("content")
             .setSmallIcon(R.drawable.btn_minus)
-            .setContentIntent(pendingIntent)
+            //.setContentIntent(pendingIntent)
             .build()
 
         startForeground(1, notification)
+        isRunning = true
+        val context = this
+        doAsync {
+            while(isRunning)
+            {
+                SystemClock.sleep(input * 10_000L)
+                uiThread {
+                    if(isRunning) {
+                        val notification = NotificationCompat.Builder(context, CHANNEL_ID_CHILD)
+                            .setContentTitle("child")
+                            .setContentText("something")
+                            .setSmallIcon(R.drawable.btn_plus)
+                            //.setContentIntent(pendingIntent)
+                            .setAutoCancel(true)
+                            .build()
+                        with(NotificationManagerCompat.from(context)) {
+                            notificationManager.notify(2, notification)
+                        }
+                    }
+                }
+            }
+        }
 
         //do heavy work on a background thread
 
@@ -53,6 +81,11 @@ class ForegroundService : Service() {
         return null
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        isRunning = false
+    }
+
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val serviceChannel = NotificationChannel(
@@ -60,10 +93,18 @@ class ForegroundService : Service() {
                 "Foreground Service Channel",
                 NotificationManager.IMPORTANCE_DEFAULT
             )
+            val serviceChannel2 = NotificationChannel(
+                CHANNEL_ID_CHILD,
+                "Foreground Service ChannelChild ",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
 
             val manager = getSystemService(NotificationManager::class.java)
             manager.createNotificationChannel(serviceChannel)
+            manager.createNotificationChannel(serviceChannel2)
         }
     }
+
+
 
 }
