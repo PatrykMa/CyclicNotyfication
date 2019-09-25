10a799a617c76a6f9caf8465c98859b812ae973f
PatrykMa <wiatraczekk@gmail.com>
2019-09-25 19:42:56 +0200

    start, stop button

--- app/src/main/java/com/example/cyclicnotyfication/MainActivity.kt
+++ app/src/main/java/com/example/cyclicnotyfication/MainActivity.kt
@@ -4,16 +4,31 @@ import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cyclicnotyfication.databinding.ActivityMainBinding
+import android.support.v4.content.ContextCompat
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        binding.buttonStart.setOnClickListener {
-
-        }
+        binding.buttonStart.setOnClickListener { startService() }
+        binding.buttonStop.setOnClickListener {stopService()  }
     }
 
+
+    private fun startService() {
+        val serviceIntent = Intent(this, ForegroundService::class.java)
+        serviceIntent.putExtra("time", 1)
+
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
+
+    private fun stopService() {
+        val serviceIntent = Intent(this, ForegroundService::class.java)
+        stopService(serviceIntent)
+    }
 
 }
